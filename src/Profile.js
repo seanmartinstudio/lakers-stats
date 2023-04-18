22972578e4176bda0ae7dc72a5a12982aa5e94fc
Sean Martin <sean@seanmartinstudio.com>
2023-04-18 14:26:46 -0700

    renders player profile info to page

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const Profile = ( {profile, team} ) => {
-    const {first_name, last_name, position, height_feet, height_inces, weight_pounds} = profile
+    const {first_name, last_name, position, height_feet, height_inches, weight_pounds} = profile
     const {full_name} = team
+
   return (
-    <div>{full_name}</div>
+    <div className='profile'>
+        <p>{first_name} {last_name}</p>
+        <p>{full_name}</p>
+        <p>Height: {height_feet}' {height_inches}"</p>
+        <p>Weight: {weight_pounds}lb.</p>
+        <p>Position: {position}</p>
+    </div>
   )
 }
 
