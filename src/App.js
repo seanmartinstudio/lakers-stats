8d84ceb8c2da53ff23805b312a84974a638fd2d6
Sean Martin <sean@seanmartinstudio.com>
2023-04-20 12:38:48 -0700

    adds api calls to player and stats endpoints, allowing player ID to pass as param to stats

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Container from './Container';
+import axios from 'axios';
 
 function App() {
-  const[profile, setProfile] = useState([])
-  const[stats, setStats] = useState([])
-  const[team, setTeam] = useState([])
+  const [profile, setProfile] = useState([])
+  const [stats, setStats] = useState([])
+  const [team, setTeam] = useState([])
+  // const [playerId, setPlayerId] = useState([])
   const [selectedPlayer, setSelectedPlayer] = useState('Shaquille Harrison')
 
   const handleSelectedPlayer = ((event) => setSelectedPlayer(event.target.value))
 
+  // useEffect(() => {
+  //   Promise.all([
+  //     fetch(`https://www.balldontlie.io/api/v1/players?search=${selectedPlayer}`),
+  //     fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[]=237'),
+  //   ])
+  //   .then(([resProfile, resStats]) => 
+  //       Promise.all([resProfile.json(), resStats.json()])
+  //     )
+  //   .then(([dataProfile, dataStats]) => {
+  //     setPlayerId(dataProfile.data[0].id)
+  //     setProfile(dataProfile.data[0])
+  //     setStats(dataStats.data[0])
+  //     setTeam(dataProfile.data[0].team)
+  //   })
+  // },[selectedPlayer])
+
   useEffect(() => {
-    Promise.all([
-      fetch(`https://www.balldontlie.io/api/v1/players?search=${selectedPlayer}`),
-      fetch('https://www.balldontlie.io/api/v1/season_averages?player_ids[]=237'),
-    ])
-    .then(([resProfile, resStats]) => 
-        Promise.all([resProfile.json(), resStats.json()])
-      )
-    .then(([dataProfile, dataStats]) => {
-      setProfile(dataProfile.data[0])
-      setStats(dataStats.data[0])
-      setTeam(dataProfile.data[0].team)
+
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${selectedPlayer}`)
+    .then(resProfile => {
+      // setPlayerId(resProfile.data.data[0].id)
+      setProfile(resProfile.data.data[0])
+      setTeam(resProfile.data.data[0].team)
+      let pId = resProfile.data.data[0].id
+      return axios.get(`https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${pId}`)
     })
+    .then(resStats => {
+      setStats(resStats.data.data[0])
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    
+
+    // .then(([dataProfile, dataStats]) => {
+    //   setPlayerId(dataProfile.data[0].id)
+    //   setProfile(dataProfile.data[0])
+    //   setStats(dataStats.data[0])
+    //   setTeam(dataProfile.data[0].team)
+    // })
   },[selectedPlayer])
 
-  console.log("Profile ->", profile)
-  console.log("Stats ->", stats)
-  console.log("Team ->", team)
+  // console.log("Profile ->", profile)
+  // console.log("Stats ->", stats)
+  // console.log("Team ->", team)
+  // console.log("Player ID ->", playerId)
 
   return (
   <div>
     <Container profile={profile} stats={stats} team={team} />
   <form>
   <label for="player-select">Select a player:</label>
+  <br></br>
   <select id="player-select" onChange={handleSelectedPlayer}>
     {/* <option value="">--Select a player--</option> */}
     <option value="Shaquille Harrison" selected>Shaquille Harrison</option>
@@ -59,12 +90,12 @@ function App() {
   )
 }
 
-//array of players if needed in the future
-const lakersRoster = [
-  "Shaquille_Harrison", "D'Angelo_Russell", "Jarred_Vanderbilt", "Anthony_Davis", "Lonnie_Walker_IV",
-  "Malik_Beasley", "LeBron_James", "Troy_Brown_Jr.", "Tristan_Thompson", "Max_Christie", "Mo_Bamba",
-  "Scotty_Pippen_Jr.", "Austin_Reaves", "Dennis_Schroder", "Cole_Swider", "Rui_Hachimura", "Wenyen_Gabriel"
-]
+//array of players as of 4/20/2023
+// const lakersRoster = [
+//   "Shaquille_Harrison", "D'Angelo_Russell", "Jarred_Vanderbilt", "Anthony_Davis", "Lonnie_Walker_IV",
+//   "Malik_Beasley", "LeBron_James", "Troy_Brown_Jr.", "Tristan_Thompson", "Max_Christie", "Mo_Bamba",
+//   "Scotty_Pippen_Jr.", "Austin_Reaves", "Dennis_Schroder", "Cole_Swider", "Rui_Hachimura", "Wenyen_Gabriel"
+// ]
 
 
 export default App;
