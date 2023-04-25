13a3b1aab717618f71ae29cf93dbbcacaebdbf53
Sean Martin <sean@seanmartinstudio.com>
2023-04-25 14:28:39 -0700

    resolves warnings

--- src/Stats.js
+++ src/Stats.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Stats = ( {stats, noStatsAvail} ) => {
-    const {games_played, player_id, season, min, fgm, fga, fg3m, fg3a, ftm, fta, oreb, dreb, reb, ast, stl, blk, turnover, pf, pts, fg_pct, fg3_pct, ft_pct} = stats
+    const {games_played, season, min, fgm, fga, fg3m, fg3a, ftm, fta, oreb, dreb, reb, ast, stl, blk, turnover, pf, pts, fg_pct, fg3_pct, ft_pct} = stats
 
   return (
     <div>
